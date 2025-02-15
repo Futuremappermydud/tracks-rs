4dbbf3547023bd5ecb1ba4c1221f99e4aeac5812
Futuremappermydud <54294576+Futuremappermydud@users.noreply.github.com>
2025-02-14 22:12:27 -0500

    Add plotting and move interpolate

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,54 @@
-use easings::functions::Functions;
+use std::{borrow::{Borrow, BorrowMut}, error::Error, time::SystemTime};
+
+use easings::{functions::Functions};
+use minifb::{Key, Window, WindowOptions};
+use plotters::{backend::BGRXPixel, chart::ChartBuilder, prelude::*, series::LineSeries, style::{Color, IntoFont, BLACK, GREEN, TRANSPARENT}};
 use serde_json::json;
 
 mod easings;
 
-fn main() {
+const W: usize = 800;
+const H: usize = 600;
+
+const FRAME_RATE: f64 = 30.0;
+
+struct BufferWrapper(Vec<u32>);
+impl Borrow<[u8]> for BufferWrapper {
+    fn borrow(&self) -> &[u8] {
+        // Safe for alignment: align_of(u8) <= align_of(u32)
+        // Safe for cast: u32 can be thought of as being transparent over [u8; 4]
+        unsafe {
+            std::slice::from_raw_parts(
+                self.0.as_ptr() as *const u8,
+                self.0.len() * 4
+            )
+        }
+    }
+}
+impl BorrowMut<[u8]> for BufferWrapper {
+    fn borrow_mut(&mut self) -> &mut [u8] {
+        // Safe for alignment: align_of(u8) <= align_of(u32)
+        // Safe for cast: u32 can be thought of as being transparent over [u8; 4]
+        unsafe {
+            std::slice::from_raw_parts_mut(
+                self.0.as_mut_ptr() as *mut u8,
+                self.0.len() * 4
+            )
+        }
+    }
+}
+impl Borrow<[u32]> for BufferWrapper {
+    fn borrow(&self) -> &[u32] {
+        self.0.as_slice()
+    }
+}
+impl BorrowMut<[u32]> for BufferWrapper {
+    fn borrow_mut(&mut self) -> &mut [u32] {
+        self.0.as_mut_slice()
+    }
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
   let testing = json!([
     [
       0,
@@ -16,6 +61,80 @@ fn main() {
     ]
     ]);
 
-  let value = easings::interpolate(0.5, Functions::EaseStep);
+  let value = Functions::EaseStep.interpolate(0.5);
   println!("Hello, world! {} {}", testing, value);
+
+  let mut window = Window::new(
+    "Tracks",
+    W,
+    H,
+    WindowOptions::default(),
+  )?;
+
+  let mut buf = BufferWrapper(vec![0u32; W * H]);
+
+  let cs = {
+    let root =
+        BitMapBackend::<BGRXPixel>::with_buffer_and_format(buf.borrow_mut(), (W as u32, H as u32))?
+            .into_drawing_area();
+    root.fill(&BLACK)?;
+
+    let mut chart = ChartBuilder::on(&root)
+        .margin(10)
+        .set_all_label_area_size(30)
+        .build_cartesian_2d(-1.2..1.2, -1.2..1.2)?;
+
+    chart
+        .configure_mesh()
+        .label_style(("sans-serif", 15).into_font().color(&GREEN))
+        .axis_style(&GREEN)
+        .draw()?;
+
+    let cs = chart.into_chart_state();
+    root.present()?;
+    cs
+  };
+
+  let start_ts = SystemTime::now();
+  let mut last_flushed = 0.0;
+
+  while window.is_open() && !window.is_key_down(Key::Escape) {
+    let epoch = SystemTime::now()
+        .duration_since(start_ts)
+        .unwrap()
+        .as_secs_f64();
+
+    if epoch - last_flushed > 1.0 / FRAME_RATE {
+        {
+            let root = BitMapBackend::<BGRXPixel>::with_buffer_and_format(
+                buf.borrow_mut(),
+                (W as u32, H as u32),
+            )?
+            .into_drawing_area();
+            {
+                let mut chart = cs.clone().restore(&root);
+                chart.plotting_area().fill(&BLACK)?;
+
+                chart
+                    .configure_mesh()
+                    .bold_line_style(&GREEN.mix(0.2))
+                    .light_line_style(&TRANSPARENT)
+                    .draw()?;
+
+                chart.draw_series(LineSeries::new(
+                  (0f64..1f64)
+                    .step(0.01)
+                    .values()
+                    .map(|x| (x, Functions::EaseInExpo.interpolate(x as f32) as f64)),
+                    &BLUE,
+                ))?;
+            }
+            root.present()?;
+        }
+
+        window.update_with_buffer(buf.borrow(), W, H)?;
+        last_flushed = epoch;
+    }
+  }
+  Ok(())
 }
