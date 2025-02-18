53272b99d8429d829c0c52fe56dd87857aeb736b
Futuremappermydud <54294576+Futuremappermydud@users.noreply.github.com>
2025-02-17 20:28:41 -0500

    Simplify alot

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,5 @@
 #![feature(let_chains)]
 
-#[cfg(target_os = "windows")]
-use minifb::{Key, Window, WindowOptions};
-#[cfg(target_os = "windows")]
-use plotters::{
-    backend::BGRXPixel,
-    chart::ChartBuilder,
-    prelude::*,
-    series::LineSeries,
-    style::{BLACK, Color, GREEN, IntoFont, TRANSPARENT},
-};
-use point_definition::{PointDefinition, float_point_definition::FloatPointDefinition};
-use serde_json::json;
-use std::{
-    borrow::{Borrow, BorrowMut},
-    error::Error,
-    time::SystemTime,
-};
-use values::base_provider_context::BaseProviderContext;
 mod easings;
 mod ffi;
 mod graphing;
@@ -27,15 +9,8 @@ mod point_definition;
 mod values;
 
 #[cfg(target_os = "windows")]
-fn main() -> Result<(), Box<dyn Error>> {
-    let testing = json!([["baseCombo", 0.0], [5.0, 1.0, "easeInOutSine"],]);
-
-    let context = BaseProviderContext::new();
-
-    let float_point_definition = FloatPointDefinition::new(&testing, &context);
-
+fn main() {
     graphing::graph("2d");
-    Ok(())
 }
 
 #[cfg(not(target_os = "windows"))]
