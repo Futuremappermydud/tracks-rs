7bcafebab9635bccc5f9095126a7ed8748c52fc5
Fernthedev <15272073+Fernthedev@users.noreply.github.com>
2025-02-20 17:17:33 -0400

    More benchmarks

--- benches/quat.rs
+++ benches/quat.rs
@@ -2,14 +2,19 @@ use criterion::{Criterion, criterion_group, criterion_main};
 use serde_json::json;
 use std::hint::black_box;
 use tracks_rs::{
-    point_definition::{PointDefinition, float_point_definition::FloatPointDefinition},
+    point_definition::{
+        PointDefinition, float_point_definition::FloatPointDefinition,
+        quaternion_point_definition::QuaternionPointDefinition,
+    },
     values::base_provider_context::BaseProviderContext,
 };
 
 fn point_step(n: u64) {
     let context = BaseProviderContext::new();
-    let definition =
-        FloatPointDefinition::new(json!([[0.0, 0.0], [1.0, 1.0, "easeInOutSine"]]), &context);
+    let definition = QuaternionPointDefinition::new(
+        json!([[0.0, 0.0, 0.0, 0.0], [1.0, 1.0, 1.0, 1.0, "easeInOutSine"]]),
+        &context,
+    );
 
     // let step = 1.0 / n as f32;
 
@@ -21,9 +26,9 @@ fn point_step(n: u64) {
 }
 
 fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("float4", |b| b.iter(|| point_step(1000)));
-    c.bench_function("float5", |b| b.iter(|| point_step(10000)));
-    c.bench_function("float6", |b| b.iter(|| point_step(100000)));
+    c.bench_function("quat4", |b| b.iter(|| point_step(1000)));
+    c.bench_function("quat5", |b| b.iter(|| point_step(10000)));
+    c.bench_function("quat6", |b| b.iter(|| point_step(100000)));
 }
 
 criterion_group!(benches, criterion_benchmark);
